9221a033f62949dd84a600811f15ca291bfac51d
Aravindhan Vijayaraj <aravindh10@gmail.com>
2016-10-25 15:04:55 -0700

    Run original solution - Takes long time when len > 5

--- anagramChecker.py
+++ anagramChecker.py
@@ -19,9 +19,16 @@ class Anagram:
 		data = [x for x in self.words if len(x)==self.minLength]
 		for ele in data:
 			anagramWords = list(map("".join,itertools.permutations(ele,self.minLength)))
+			anagramWords = set(anagramWords);
 			self.checkAnagram(anagramWords,data);
 
-	def checkAnagram(self):
+	def checkAnagram(self,wordlist,dictionary):
+		anagramList = []
+		for entry in wordlist:
+			if entry in dictionary:
+				anagramList.append(entry)
+		if len(anagramList) >= self.minLength:
+			print anagramList
 
 	def main(self):
 		self.loadFile()
@@ -29,8 +36,10 @@ class Anagram:
 		maxLen = len(max(self.words,key = len))
 		#print maxLen
 		while(self.minLength <= maxLen):
-			self.formAnagram(self.words,self.minLength)
+			print "---------------"+str(self.minLength)+"----------------------"
+			self.formAnagram()
 			self.minLength += 1
+			#break
 
 
 if __name__=='__main__' :
