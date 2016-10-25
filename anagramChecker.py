b8821e22d71252eb32dfa4e8824953fc528dff4b
Aravindhan Vijayaraj <aravindh10@gmail.com>
2016-10-25 13:51:23 -0700

    Form list of words for each word length and get all permutations of a word

--- anagramChecker.py
+++ anagramChecker.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 
 FILE_LOC = "/usr/share/dict/words"
@@ -11,13 +12,28 @@ class Anagram:
 	def loadFile(self):
 		self.words = np.loadtxt(FILE_LOC,dtype = 'str')
 		self.words = [x.lower() for x in self.words if x.isalpha()]
-		print len(self.words)
+		#print len(self.words)
+
+
+	def formAnagram(self):
+		data = [x for x in self.words if len(x)==self.minLength]
+		for ele in data:
+			anagramWords = list(map("".join,itertools.permutations(ele,self.minLength)))
+			self.checkAnagram(anagramWords,data);
+
+	def checkAnagram(self):
 
 	def main(self):
-		self.loadFile();
+		self.loadFile()
+		#print len(self.words)
+		maxLen = len(max(self.words,key = len))
+		#print maxLen
+		while(self.minLength <= maxLen):
+			self.formAnagram(self.words,self.minLength)
+			self.minLength += 1
 
 
 if __name__=='__main__' :
-	anagramObj = Anagram();
-	anagramObj.main();	
+	anagramObj = Anagram()
+	anagramObj.main()
     
