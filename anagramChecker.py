63c98785a31a0d8979b1f381b91279910284ebf3
Aravindhan Vijayaraj <aravindh10@gmail.com>
2016-10-25 15:15:20 -0700

    Hashmap function with prime numbers for each char

--- anagramChecker.py
+++ anagramChecker.py
@@ -1,13 +1,30 @@
 import numpy as np
 import itertools
-
+import string
 
 FILE_LOC = "/usr/share/dict/words"
+
 
 class Anagram:
 
 	def __init__(self, minLength = 4) :
 		self.minLength = minLength
+
+	def hashmap(self):
+		primeVal = [2]
+		ele = 3
+		while len(primeVal) < 26:
+			result = []
+			for n in primeVal:
+				result.append(ele % n)
+			if 0 not in result:
+				primeVal.append(ele)
+			ele += 2
+		print primeVal
+		charVals = list(string.ascii_lowercase)
+		self.hashFn = dict(zip(charVals,primeVal));
+		print self.hashFn
+
 		
 	def loadFile(self):
 		self.words = np.loadtxt(FILE_LOC,dtype = 'str')
@@ -32,14 +49,16 @@ class Anagram:
 
 	def main(self):
 		self.loadFile()
-		#print len(self.words)
+		'''print len(self.words)
 		maxLen = len(max(self.words,key = len))
-		#print maxLen
+		print maxLen
 		while(self.minLength <= maxLen):
 			print "---------------"+str(self.minLength)+"----------------------"
 			self.formAnagram()
 			self.minLength += 1
-			#break
+			#break'''
+		self.hashmap()
+
 
 
 if __name__=='__main__' :
